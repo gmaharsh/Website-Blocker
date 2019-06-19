ac38f467058ca87a154f6d0e89242cc18f9100b2
gmaharsh <gmaharsh@yahoo.com>
2019-06-19 09:42:44 +0530

    Comparing Time

--- website-blocker.py
+++ website-blocker.py
@@ -1,4 +1,7 @@
 import time
+from datetime import datetime as dt
+
+
 hosts_path ="C:\Windows\System32\drivers\etc\hosts"
 
 redirect = "127.0.0.1"
@@ -6,5 +9,9 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now():
+        print("Working Hours")
+
+    else:
+        print("Working Hours")
     time.sleep(5)
