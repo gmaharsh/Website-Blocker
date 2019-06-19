7ee91eb22d35c6fe5857556cdf1396bb8ee04c11
gmaharsh <gmaharsh@yahoo.com>
2019-06-19 09:55:19 +0530

    Adding contents to hosts file

--- website-blocker.py
+++ website-blocker.py
@@ -1,17 +1,23 @@
 import time
 from datetime import datetime as dt
 
-
+host_temp = "hosts.txt"
 hosts_path ="C:\Windows\System32\drivers\etc\hosts"
 
 redirect = "127.0.0.1"
 
-website_list = ["www.facebook.com", "facebook.com"]
+website_list = ["www.facebook.com", "https://www.facebook.com/"]
 
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now():
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() <dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print("Working Hours")
-
+        with open(host_temp,'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
     else:
         print("Working Hours")
     time.sleep(5)
